184fbc22bed9306a440cf5e496df56066ce1f7c9
oykot <45454113+oykot@users.noreply.github.com>
2021-05-24 21:04:34 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 apt update && apt upgrade
 apt -y install apache2 git
+cd /etc/apache2/sites-available/
+curl -O https://raw.githubusercontent.com/oykot/bao/main/000-default.conf
+systemctl restart apache2
 cd /var/www/html/
 git clone https://github.com/oykot/bao.git
