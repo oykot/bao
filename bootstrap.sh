04373716345332a3b90cd39b74e253efce363926
oykot <45454113+oykot@users.noreply.github.com>
2021-05-24 21:12:38 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,4 +5,5 @@ cd /etc/apache2/sites-available/
 curl -O https://raw.githubusercontent.com/oykot/bao/main/000-default.conf
 systemctl restart apache2
 cd /var/www/html/
+rm -Rf /var/www/html/bao/
 git clone https://github.com/oykot/bao.git
