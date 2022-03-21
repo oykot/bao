38b7228b847c1b8f7e904724c04e07ba4dc107a5
oykot <45454113+oykot@users.noreply.github.com>
2022-03-21 22:20:23 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-apt update && apt upgrade
+apt -y update
+apt -y upgrade
 apt -y install apache2 git stress-ng
 cd /etc/apache2/sites-available/
 curl -O https://raw.githubusercontent.com/oykot/bao/main/000-default.conf
 systemctl restart apache2
 cd /var/www/html/
-rm -Rf /var/www/html/bao/
+mkdir /var/www/html/bao/
 git clone https://github.com/oykot/bao.git
+cat <<EOF > /var/www/html/bao/index.html
+<html><body><h1>$HOSTNAME</h1></body></html>
+<img src="https://storage.googleapis.com/storedata-load-balance-web-flow/logo_SdV.png">
+EOF
