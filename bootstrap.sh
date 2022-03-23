29f354b72c27f2fa49be106528b3efe457121637
oykot <45454113+oykot@users.noreply.github.com>
2022-03-23 16:51:14 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ mkdir /var/www/html/bao/
 # git clone https://github.com/oykot/bao.git
 cat <<EOF > /var/www/html/bao/index.html
 <html><body><h1>$HOSTNAME</h1></body></html>
-<html><body><h1>La v3 de mon app</h1></body></html>
-<img src="https://storage.googleapis.com/storedata-load-balance-web-flow/logo_SdV.png">
+<html><body><h1>La v4 de mon app</h1></body></html>
+<img src="https://storage.googleapis.com/storedata-projet-web-345015/chaos-monkey-2.png">
 EOF
 # apt -y upgrade
